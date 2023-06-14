17a4e5ecd3a18391d3249c486a0f7e49832ed62c
Hardik Anand <hardik.a@copperdigital.com>
2023-06-14 20:42:31 +0530

    Made Admin Navigate Loading faster by Navigating first and then setup cookie

--- src/app/Services/auth-service.service.ts
+++ src/app/Services/auth-service.service.ts
@@ -16,16 +16,12 @@ export class AuthServiceService {
     return this.http.post<any>('https://sis.hi-line.com/dev/api/api/user/login', body, { headers });
   }
   setToken(token: string, rememberMe: boolean) {
-    this.token = token;
-    this.setCookie(token, rememberMe)
     this.router.navigate(['/admin']);
-  }
-  async setCookie(token: string, rememberMe: boolean) {
-    let run = await Promise.resolve(rememberMe)
-    if (run) {
+    this.token = token;
+    if (rememberMe) {
       const expirationDate = new Date();
       expirationDate.setDate(expirationDate.getDate() + 30);
-      await this.cookieService.set('loginToken', token, expirationDate);
-    } else await this.cookieService.set('loginToken', token, 0);
+      this.cookieService.set('loginToken', token, expirationDate);
+    } else this.cookieService.set('loginToken', token, 0);
   }
 }
