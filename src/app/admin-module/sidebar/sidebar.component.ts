bd3ea0efbfa4a3c43a63ff6a8d16bab5dbbac223
Hardik Anand <hardik.a@copperdigital.com>
2023-06-26 18:46:21 +0530

    Sidebar styling added to according to routing

--- src/app/admin-module/sidebar/sidebar.component.ts
+++ src/app/admin-module/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,17 +7,27 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  activeRoute = [{ activeState: true, path: '', blackUrl: '/assets/Icons/Black_Sidebar/Inventory.svg', whiteUrl: '/assets/Icons/White_Sidebar/Inventory.svg' },
+  activeRoute = [{ activeState: false, path: '', blackUrl: '/assets/Icons/Black_Sidebar/Inventory.svg', whiteUrl: '/assets/Icons/White_Sidebar/Inventory.svg' },
   { activeState: false, path: 'users', blackUrl: '/assets/Icons/Black_Sidebar/Team.svg', whiteUrl: '/assets/Icons/White_Sidebar/Team.svg' },
   { activeState: false, path: 'price_and_parts', blackUrl: '/assets/Icons/Black_Sidebar/Price.svg', whiteUrl: '/assets/Icons/White_Sidebar/Price.svg' },
   { activeState: false, path: 'area', blackUrl: '/assets/Icons/Black_Sidebar/Area.svg', whiteUrl: '/assets/Icons/White_Sidebar/Area.svg' },
   { activeState: false, blackUrl: '/assets/Icons/Black_Sidebar/Parts.svg', whiteUrl: '/assets/Icons/White_Sidebar/Parts.svg' }]
-  constructor(private router: Router, private route: ActivatedRoute) { }
-  changeActiveRoute(activeIndex: number, path: string | undefined) {
-    this.activeRoute.forEach((route, index) => {
-      if (index === activeIndex) route.activeState = true;
-      else route.activeState = false;
-    })
+  constructor(private router: Router, private route: ActivatedRoute) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.activeRoute.forEach((route) => {
+          let routePath = '';
+          if (route.path != '') routePath = `/${route.path}`;
+          if (event.url === `/admin${routePath}`) route.activeState = true;
+          else route.activeState = false
+        })
+        console.log('Path changed:', event.url);
+        // Do whatever you need to do here
+      }
+    });
+  }
+  changeActiveRoute(path: string | undefined) {
     this.router.navigate([path], { relativeTo: this.route });
   }
+
 }
